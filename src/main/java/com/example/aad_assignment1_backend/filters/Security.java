e6c478c36c7c4cc49b0979f821d08d9e88b3c1ef
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-08-22 17:26:44 +0530

    security

--- src/main/java/com/example/aad_assignment1_backend/filters/Security.java
+++ src/main/java/com/example/aad_assignment1_backend/filters/Security.java
@@ -1,4 +1,17 @@
 package com.example.aad_assignment1_backend.filters;
 
-public class Security {
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Security extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Security");
+        chain.doFilter(req,res);
+    }
 }
