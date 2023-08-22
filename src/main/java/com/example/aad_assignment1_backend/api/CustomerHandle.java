7c2e33ea0ddf71e8f5e43c8cfa19872acd5084c8
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-08-22 16:32:20 +0530

    api

--- src/main/java/com/example/aad_assignment1_backend/api/CustomerHandle.java
+++ src/main/java/com/example/aad_assignment1_backend/api/CustomerHandle.java
@@ -1,4 +1,11 @@
 package com.example.aad_assignment1_backend.api;
 
-public class CustomerHandle {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.sql.Connection;
+
+@WebServlet(name = "CustomerHandle", value = "/CustomerHandle")
+public class CustomerHandle extends HttpServlet{
+
+
 }
