335eb18338b078cdcc00e6bec531c48e1d5e8f2f
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-08-22 17:25:23 +0530

    log

--- src/main/java/com/example/aad_assignment1_backend/filters/Log.java
+++ src/main/java/com/example/aad_assignment1_backend/filters/Log.java
@@ -1,4 +1,16 @@
 package com.example.aad_assignment1_backend.filters;
 
-public class Log {
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Log extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Log filter");
+        chain.doFilter(req,res);
+    }
 }
